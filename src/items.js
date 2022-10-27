26d6ade5b593b60a9681334e7387f68b89eb97ae
Irina Eliseeva <irina.eliseeva1991@gmail.com>
2022-10-27 20:12:55 +0300

    loading HM3

---
+++ src/items.js
@@ -0,0 +1,59 @@
+import image1 from "../media/img/1.jpg";
+import image2 from "../media/img/2.jpg";
+import image3 from "../media/img/3.jpg";
+import image4 from "../media/img/4.jpg";
+
+import audio1 from "../media/audio/birds01.mp3";
+import audio2 from "../media/audio/birds02.mp3";
+import audio3 from "../media/audio/birds03.mp3";
+
+import video1 from "../media/video/1.mp4";
+import video2 from "../media/video/2.mp4";
+
+export default [
+  {
+    resource: image1,
+    type: "image",
+    title: "Книга и чашка кофе"
+  },
+  {
+    resource: image2,
+    type: "image",
+    title: "Полевые цветы"
+  },
+  {
+    resource: image3,
+    type: "image",
+    title: "Зеленый декор"
+  },
+  {
+    resource: image4,
+    type: "image",
+    title: "Спелый ананас"
+  },
+  {
+    resource: audio1,
+    type: "audio",
+    title: "Поют птицы утром"
+  },
+  {
+    resource: audio2,
+    type: "audio",
+    title: "Поют птицы днем"
+  },
+  {
+    resource: audio3,
+    type: "audio",
+    title: "Поют птицы ночью"
+  },
+  {
+    resource: video1,
+    type: "video",
+    title: "Ноне"
+  },
+  {
+    resource: video2,
+    type: "video",
+    title: "Морской прибой"
+  }
+]
